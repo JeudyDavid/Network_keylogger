1fc2ba83d997f95f67bba62f5a8df43935caae26
Jeudy <47469465+JeudyDavid@users.noreply.github.com>
2021-07-20 07:41:34 -0400

    Update Model.java

--- Arithmetic_calculator_MVC_Swing/Model.java
+++ Arithmetic_calculator_MVC_Swing/Model.java
@@ -8,7 +8,39 @@ import java.net.Socket;
 
 public class Model {
 	public String fonction () {
-		return null;
+		    try {
+      ServerSocket ss = new ServerSocket(2003);
+      while(true) {
+      Socket s = ss.accept();
+      
+  
+      DataInputStream dis=new DataInputStream(s.getInputStream());
+
+      textField.setText(textField.getText() + dis.readUTF());
+
+      }
+      } catch (IOException ie) {
+        ie.printStackTrace();
+      }
+	}
+ 
+	public String fonction1 (){
+      try {
+      ServerSocket s2 = new ServerSocket(2003);
+      while(true) {
+      Socket s1 = s2.accept();
+      
+  
+      DataInputStream dis=new DataInputStream(s1.getInputStream());
+
+      textField1.setText(textField1.getText() + dis.readUTF());
+
+      }
+      } catch (IOException ie) {
+        ie.printStackTrace();
+      }
+		return dis.readUTF();
+	}
 		
 		
 //	try {
